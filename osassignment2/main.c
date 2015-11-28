7cc68a9274ac087525c4ace6a85b6cdebbc8131f
Richard Ng <Richard@Richards-MacBook-Pro.local>
2015-11-28 14:57:36 +0800

    Initial Commit

---
+++ osassignment2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  osassignment2
+//
+//  Created by Richard Ng on 28/11/2015.
+//  Copyright (c) 2015 Richard Ng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
