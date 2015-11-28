ba66f5339346c650b8d651ac365bf4693fd0b4a9
Richard Ng <Richard@Richards-MacBook-Pro.local>
2015-11-28 16:45:55 +0800

    Add mutex Add pthreads Pthreads work

--- osassignment2/main.c
+++ osassignment2/main.c
@@ -6,12 +6,62 @@
 //  Copyright (c) 2015 Richard Ng. All rights reserved.
 //
 
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
+void* readerThread(void *arg);
+void* writerThread(void *arg);
+
+#define buffsize 1024
+char fileBuffer[buffsize];
+
+pthread_t threads[6];
+pthread_mutex_t mutex;
+
+FILE* fileReader;
+FILE* filePtr2;
+
+
+void *readerThread()
+{
+    pthread_mutex_lock(&mutex);
+    while(fgets(fileBuffer,buffsize,fileReader))
+    {
+        printf("%s",fileBuffer);
+    }
+    pthread_mutex_unlock(&mutex);
+    return 0;
+}
+
+
+int main()
+{
+    fileReader = fopen("/Users/Richard/Documents/Xcode/osassignment2/input.txt","r");
+    filePtr2 = fopen("/Users/Richard/Documents/Xcode/osassignment2/output.txt","w");
     
-    FILE* filePtr1 = fopen("input.txt","r");
-    FILE* filePtr2 = fopen("output.txt","w");
+    pthread_mutex_init(&mutex, NULL);
     
+    for (int i = 0; i<3; i++)
+    {
+        
+    pthread_create(&threads[i],NULL ,&readerThread,NULL);
+    }
+    
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+    pthread_join(threads[2], NULL);
+    
+    fclose(filePtr2);
+    fclose(fileReader);
+    
+    pthread_mutex_destroy(&mutex);
+    pthread_exit(NULL);
+    
+    
+    
+   
     
 }
+
+
